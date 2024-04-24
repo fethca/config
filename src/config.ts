fb78c8363b0b16d71858336c990cf174e11a1c46
Fethca <31514619+fethca@users.noreply.github.com>
2024-04-24 10:59:24 +0200

    feat: config service (#1)

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+import { EventEmitter } from 'stream'
+
+export abstract class ConfigService<T> extends EventEmitter {
+  private config?: T
+  private expiryTime = 0
+
+  constructor(private interval: number) {
+    super()
+  }
+
+  abstract fetch(): Promise<T>
+
+  async getConfig(force = false): Promise<T> {
+    if (force || !this.config || this.isExpired()) {
+      try {
+        this.config = await this.fetch()
+      } catch (error) {
+        this.emit('fetch-error', error)
+      }
+      this.updateExpired()
+    }
+
+    if (!this.config) {
+      throw new Error('Config could not be loaded')
+    }
+
+    return this.config
+  }
+
+  private isExpired(): boolean {
+    return this.expiryTime < Date.now()
+  }
+
+  private updateExpired() {
+    this.expiryTime = Date.now() + this.interval
+  }
+}
